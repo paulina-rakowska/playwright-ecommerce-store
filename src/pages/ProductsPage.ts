0d0fa77000829823db1f33c4d0c8dfadd56e936d
Paulina Rakowska <paulina.m.rakowska@gmail.com>
2025-11-21 22:57:19 +0100

    Grouping tests

--- src/pages/ProductsPage.ts
+++ src/pages/ProductsPage.ts
@@ -17,10 +17,6 @@ export default class ProductsPage extends BasicPage {
         const product = index === 1 ? this.page.locator(".inventory_item").first() : this.page.locator(".inventory_item"); // to do index != 1
         return product;
     }
-    async getProductButton(product: Locator): Promise<Locator> {
-        const addToCartButton = product?.locator("button");
-        return addToCartButton;
-    }
     async getProductsCount(): Promise<number> {
         await this.getProducts();
         const count = await this.productElements!.count();
@@ -32,14 +28,12 @@ export default class ProductsPage extends BasicPage {
         await addToCartButton?.click();
         return addToCartButton;
     }
-    async addProductByName(productName: string): Promise<void> {
-        console.log(this.productElements);
-        console.log(productName);
-       // const selectedProduct = this.productElements?.find((product) => product.);
-    }   
     async addRandomProductToCart(): Promise<void> {
         const randomProduct = this.productElements?.nth(Math.floor(Math.random() * this.productCount!));
         const addToCartButton = randomProduct?.locator("button");
         await addToCartButton?.click();
     }
+    async addAllProductsToCart(): Promise<void> {
+        console.log(this.productElements);
+    }   
 }
--- tests/add_to_cart.spec.ts
+++ tests/add_to_cart.spec.ts
@@ -3,36 +3,37 @@ import { inventoryUrl } from '../src/utils/env';
 import ProductsPage from '../src/pages/ProductsPage';
 
 test.describe('add to cart scenarios', () => {
-    let testedPage: ProductsPage;
-    let cartBadgeElement: Locator;
-    let cartBadgeCount: number;
 
-    test('add first product to cart on inventory page', async ({ page }) => {
-        let firstProduct: Locator;
-        let firstProductButton: Locator;
+    test.describe('add to cart on inventory page', () => {
+        let testedPage: ProductsPage;
+        let cartBadgeElement: Locator;
+        let cartBadgeCount: number;
 
-        await test.step(`Go to the inventory page`, async () => {
+        test.beforeEach(async ({ page }) => {
             const productsPage = new ProductsPage(page);
             productsPage.gotoTheStore(inventoryUrl);
             cartBadgeElement =  page.locator('[data-test="shopping-cart-link"]');
             cartBadgeCount = parseInt((await cartBadgeElement.textContent()) || '0');
             testedPage = productsPage;
         });
-        await test.step(`Get first product`, async () => {
-            firstProduct = await testedPage.getProductByIndex(1);
-            expect(firstProduct).toBeEnabled();
-        });
-        await test.step(`Click first product`, async () => {
-            firstProductButton = await testedPage.addFirstProductToCart(firstProduct);
-        });
-        await test.step(`Check if cart badge count is incremented`, async () => {
-           let newCount = parseInt((await cartBadgeElement.textContent()) || '0'); 
-           expect(newCount).toEqual(cartBadgeCount + 1);
-        });
-        await test.step(`Check if button text changed to Remove`, async () => {
-            await expect(firstProductButton).toHaveText('Remove');
+        test('add first product to cart on inventory page', async ({ page }) => {
+            let firstProduct: Locator;
+            let firstProductButton: Locator;
+
+            await test.step(`Get first product`, async () => {
+                firstProduct = await testedPage.getProductByIndex(1);
+                expect(firstProduct).toBeEnabled();
+            });
+            await test.step(`Click first product`, async () => {
+                firstProductButton = await testedPage.addFirstProductToCart(firstProduct);
+            });
+            await test.step(`Check if cart badge count is incremented`, async () => {
+            let newCount = parseInt((await cartBadgeElement.textContent()) || '0'); 
+            expect(newCount).toEqual(cartBadgeCount + 1);
+            });
+            await test.step(`Check if button text changed to Remove`, async () => {
+                await expect(firstProductButton).toHaveText('Remove');
+            });
         });
     });
-
-
 });
